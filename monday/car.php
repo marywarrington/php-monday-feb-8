8564436c3b9f972896cd13d3731d62246b5a0e16
Jon Nardine and Mary Warrington <marywarrington@gmail.com>
2016-02-08 10:23:30 -0800

    change price to private

---
+++ monday/car.php
@@ -0,0 +1,60 @@
+<?php
+class Car
+{
+    public $make_model;
+    private $price;
+    public $miles;
+    public $color;
+    public $condition;
+
+    function __construct($make_model, $car_price, $miles, $color, $condition = "Used")
+    {
+        $this->make_model = $make_model;
+        $this->price = $car_price;
+        $this->miles = $miles;
+        $this->color = $color;
+        $this->condition = $condition;
+    }
+    function getPrice()
+    {
+        return $this->price;
+    }
+}
+
+$porsche = new Car("2014 Porsche 911", 114991, 7864, "Blue", "New");
+$ford = new Car("2011 Ford F450", 55995, 14241, "Maroon");
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000, "Silver");
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "Green");
+
+$cars = array($porsche, $ford, $lexus, $mercedes);
+
+$cars_matching_search = array();
+foreach ($cars as $car) {
+    if ($car->getPrice() < $_GET["price"]) {
+        array_push($cars_matching_search, $car);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Car Dealership's Homepage</title>
+</head>
+<body>
+    <h1>Your Car Dealership</h1>
+    <ul>
+        <?php
+            foreach ($cars_matching_search as $car) {
+              $car_price = $car->getPrice();
+                echo "<li> $car->make_model </li>";
+                echo "<ul>";
+                    echo "<li> $$car_price </li>";
+                    echo "<li> Miles: $car->miles </li>";
+                    echo "<li> Condition: $car->condition </li>";
+                echo "</ul>";
+            }
+        ?>
+    </ul>
+</body>
+</html>
